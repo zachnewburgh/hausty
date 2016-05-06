c68de25550d0cf8fea994b1de8e2d6124f7f1416
zachnewburgh <zach.newburgh@gmail.com>
2016-05-06 10:49:21 -0400

    mocked up a #rating for apartment class

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,4 +1,14 @@
 class Apartment < ActiveRecord::Base
   has_many :reviews
   has_many :users, through: :reviews
+
+  # def rating
+  #   rating_sum = 0
+  #   rating_count = 0
+  #   Apartment.find_by(id: params[:id]).reviews.each do |review|
+  #     rating_sum += review.rating.to_i
+  #     rating_count += 1
+  #   end
+  #   rating_avg = rating_sum/rating_count
+  # end
 end
